1ab96d226c890979db16af906153f4d45b787a4e
Daniel Frydman <danielfrydman@id.uff.br>
2021-10-30 16:26:27 -0300

    Tarefa 01 com o solicitado e incremento de cor

--- Tarefa01HelloWorldNoHeroku/src/main/java/hello/HelloServlet.java
+++ Tarefa01HelloWorldNoHeroku/src/main/java/hello/HelloServlet.java
@@ -1,9 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hello;
+
+import java.util.Calendar;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,129 +10,178 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author viter
- */
 @WebServlet("/alomundo")
 public class HelloServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet HelloServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet HelloServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        String msg = "";
-        
-        String lang = request.getParameter("lang");
-        if(lang==null)
-            lang = "pt";
-        switch(lang){
-            case "pt":
-                msg = "Alô, ";
-                break;
-            case "en":
-                msg = "Hello, ";
-                break;
-            case "fr":
-                msg = "Bonjour, ";
-                break;
-        }
-        
-        String nome = request.getParameter("nome");
-
-        if(nome==null)
-            nome = "Fulano";
-        
-        msg = msg+nome+"!";
-
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet HelloServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet HelloServlet</h1>");
-            out.println("<p>" + msg + "</p>");
-            out.println("</body>");
-            out.println("</html>");
-        }
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String msg = "";
+        Calendar c1 = Calendar.getInstance();
+	int hora = c1.get(Calendar.HOUR_OF_DAY);
+        
+        String msg = "", trat = "";
         
         String lang = request.getParameter("lang");
         if(lang==null)
             lang = "pt";
-        switch(lang){
-            case "pt":
-                msg = "Alô, ";
-                break;
-            case "en":
-                msg = "Hello, ";
-                break;
-            case "fr":
-                msg = "Bonjour, ";
-                break;
-            case "de":
-                msg = "Hallo, ";
-                break;
-        }
+        
+        String tratamento = request.getParameter("tratamento");
+        if(tratamento==null)
+            tratamento = "";
+        
+        String favcolor = "color:" + request.getParameter("favcolor") + ";";
+        
+        if(hora > 6 && hora < 12){
+            switch(lang){
+                case "pt":
+                    if (tratamento.equals("sr"))
+                        trat = "Senhor ";
+                    else
+                        trat = "Senhora ";
+                    msg = "Bom Dia, ";
+                    break;
+                case "en":
+                    if (tratamento.equals("sr"))
+                        trat = "Sir ";
+                    else
+                        trat = "Madam ";
+                    msg = "Good Morning, ";
+                    break;
+                case "fr":
+                    if (tratamento.equals("sr"))
+                        trat = "Monsieur ";
+                    else
+                        trat = "Madame ";
+                    msg = "Bonjour, ";
+                    break;
+                case "de":
+                    if (tratamento.equals("sr"))
+                        trat = "Zotëri ";
+                    else
+                        trat = "Zonja ";
+                    msg = "Guten Morgen, ";
+                    break;
+                case "he":
+                    if (tratamento.equals("sr"))
+                        trat = " אֲדוֹנִי";
+                    else
+                        trat = "ג ְבֶרֶת";
+                    msg = " בוקר טוב";
+                    break;
+                case "es":
+                    if (tratamento.equals("sr"))
+                        trat = "Señor ";
+                    else
+                        trat = "Señora ";
+                    msg = "Buen día, ";
+                    break;
+            }
+        }else if(hora > 12 && hora < 18){
+            switch(lang){
+                case "pt":
+                    if (tratamento.equals("sr"))
+                        trat = "Senhor ";
+                    else
+                        trat = "Senhora ";
+                    msg = "Boa tarde, ";
+                    break;
+                case "en":
+                    if (tratamento.equals("sr"))
+                        trat = "Sir ";
+                    else
+                        trat = "Madam ";
+                    msg = "Good afternoon, ";
+                    break;
+                case "fr":
+                    if (tratamento.equals("sr"))
+                        trat = "Monsieur ";
+                    else
+                        trat = "Madame ";
+                    msg = "Bon après-midi, ";
+                    break;
+                case "de":
+                    if (tratamento.equals("sr"))
+                        trat = "Zotëri ";
+                    else
+                        trat = "Zonja ";
+                    msg = "Guten Tag, ";
+                    break;
+                case "he":
+                    if (tratamento.equals("sr"))
+                        trat = " אֲדוֹנִי";
+                    else
+                        trat = " גְבֶרֶת";
+                    msg = " אחר הצהריים טובים";
+                    break;
+                case "es":
+                    if (tratamento.equals("sr"))
+                        trat = "Señor ";
+                    else
+                        trat = "Señora ";
+                    msg = "Buenas tardes, ";
+                    break;
+            }
+        }else{
+            switch(lang){
+                case "pt":
+                    if (tratamento.equals("sr"))
+                        trat = "Senhor ";
+                    else
+                        trat = "Senhora ";
+                    msg = "Boa noite, ";
+                    break;
+                case "en":
+                    if (tratamento.equals("sr"))
+                        trat = "Sir ";
+                    else
+                        trat = "Madam ";
+                    msg = "Good night, ";
+                    break;
+                case "fr":
+                    if (tratamento.equals("sr"))
+                        trat = "Monsieur ";
+                    else
+                        trat = "Madame ";
+                    msg = "Bonne nuit, ";
+                    break;
+                case "de":
+                    if (tratamento.equals("sr"))
+                        trat = "Zotëri ";
+                    else
+                        trat = "Zonja ";
+                    msg = "Naten e mire, ";
+                    break;
+                case "he":
+                    if (tratamento.equals("sr"))
+                        trat = "א ֲדוֹנִי";
+                    else
+                        trat = " גְבֶרֶת";
+                    msg = " לילה טוב";
+                    break;
+                case "es":
+                    if (tratamento.equals("sr"))
+                        trat = "Señor ";
+                    else
+                        trat = "Señora ";
+                    msg = "Buenas noches, ";
+                    break;
+            }
+	}
         
         String nome = request.getParameter("nome");
 
         if(nome==null)
             nome = "Fulano";
         
-        msg = msg+nome+"!";
+        if (lang.equals("he"))
+            msg = "!"+nome+trat+msg;
+        else
+            msg = msg+trat+nome+"!";
 
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
@@ -147,20 +193,10 @@ public class HelloServlet extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet HelloServlet</h1>");
-            out.println("<p>" + msg + "</p>");
+            out.println("<p style="+favcolor+">" + msg +"</p>");
+            out.println("<a href=\".\\helloform.html\">Voltar</a>");
             out.println("</body>");
             out.println("</html>");
         }
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
